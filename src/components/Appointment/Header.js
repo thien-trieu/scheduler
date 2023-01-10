c081ceebf4d95a394a37018803cf7af54955094f
Thien Trieu <thien.trieu@telus.com>
2023-01-09 20:29:09 -0800

    Add header component

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import classNames from "classnames";
+
+// import "./styles.scss";
+
+export default function Header(props) {
+  return <header className="appointment__time">
+    <h4 className="text--semi-bold">12pm</h4>
+    <hr className="appointment__separator" />
+  </header>;
+}
