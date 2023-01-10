361a980a73e5cc0ab26500126e20d6d4e2fd8c89
Thien Trieu <thien.trieu@telus.com>
2023-01-10 12:46:44 -0800

    Add stories for appointment empty and appointment booked. used props for header and show components in Appointment index.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,11 @@ import "./styles.scss";
 
 // Main container for appointment 
 export default function Appointment(props) {
+  
   return (
-  <article className="appointment"></article>
+  <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+  </article>
   );
 }
