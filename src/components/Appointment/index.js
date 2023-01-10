32c822971e29d6b10168923ff8a970e078609290
Thien Trieu <thien.trieu@telus.com>
2023-01-10 11:51:40 -0800

    import header, show, empty components into apppointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "./styles.scss";
 
 // Main container for appointment 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+  <article className="appointment"></article>
+  );
 }
