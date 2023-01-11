2eb9dfe088ed605b14a896d857aa486d7c6eaa2c
Thien Trieu <thien.trieu@telus.com>
2023-01-11 13:58:52 -0800

    update useEffect to use Promise.all to call all the APIs and update data in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,47 +2,9 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList.js";
 import Appointment from "./Appointment/index.js";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors.js";
 
 import "components/Application.scss";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
 
 export default function Application(props) {
   
@@ -53,23 +15,27 @@ export default function Application(props) {
     appointments: {}
   });
 
-  // setDay and setDays function kept separately to call setState to update day or days
-  // only update day in state
+  // update day in state
   const setDay = day => setState({ ...state, day });
-  
-  // only update days in state
-  const setDays = (days) => {
-    // copy previous days data and update with new days data
-    setState(prev => ({ ...prev, days }));
-  };
 
-  // get all the days data from API
+  // get daily appointments for days from state object
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
+
+  // get all the data from API
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-      .then(res => {
-        console.log(res.data);
-        setDays([...res.data]);
-      });
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then((all) => {
+ 
+      const days = all[0].data
+      const appointments = all[1].data
+
+      // update state object with days and appointments from api
+      setState(prev => ({...prev, days, appointments}))
+    }).catch(err => console.log(err))
+
   }, []);
 
   return (
@@ -95,7 +61,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return (
             <Appointment key={appointment.id}  {...appointment} />
           );
