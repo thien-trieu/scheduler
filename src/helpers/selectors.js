f62a3ed461cf603672d5b9fb2366e33ed2415982
Thien Trieu <thien.trieu@telus.com>
2023-01-11 10:32:09 -0800

    getAppointmentsForDay function test refactored

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,22 +1,30 @@
 export function getAppointmentsForDay(state, day) {
-
+  //... returns an array of appointments for that day
   const result = [];
+
+  // if no days data
   if (!state.days.length) {
     return result;
   }
+  // filter through state for a specific day
+  const getDay = state.days.filter(x => x.name === day);
 
-  const selectedDay = state.days.filter(
-    stateDay => stateDay.name === day)[0];
-  if (!selectedDay) {
+  // if no data for a specific day
+  if (!getDay[0]) {
     return result;
   }
 
-  selectedDay.appointments.forEach(appointment => {
-   
-    if (appointment === state.appointments[appointment].id) {
-      result.push(state.appointments[appointment]);
+  // get the appointments IDs for that day
+  const appointmentIdsForDay = getDay[0].appointments;
+
+  // get  details for all appointments
+  const appointments = state.appointments;
+
+  // get appointment details for the specific day if id matches
+  appointmentIdsForDay.forEach(id => {
+    if (id === appointments[id].id) {
+      result.push(appointments[id]);
     }
- 
   });
 
   return result;
