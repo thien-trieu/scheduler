cf358070c806c0ece55571f2de7bfbcca53175af
Thien Trieu <thien.trieu@telus.com>
2023-01-11 22:31:25 -0800

    import useVisualMode to appointment/index.js. Add EMPTY,SHOW,CREATE constants for mode.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,16 +2,36 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
 
+
 // Main container for appointment 
 export default function Appointment(props) {
-  
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE ="CREATE"
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
-  <article className="appointment">
-    <Header time={props.time}/>
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
-  </article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+        />
+      
+      )}
+      {mode === CREATE && <Form interviewers={[]} onSave={()=>{}} onCancel={()=>{}}/>}
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -152,7 +152,8 @@ storiesOf("Appointment", module)
   .add("Show", ()=> <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", ()=> <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", ()=> <Status message="Deleting"/>)
-  .add("Error", ()=> <Error message="Could not delete appointment." onClose={action("onClose")}/>)  .add("Create", ()=> <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Error", ()=> <Error message="Could not delete appointment." onClose={action("onClose")}/>) 
+  .add("Create", ()=> <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
   .add("Edit", ()=> <Form student="Kim" interviewer={3} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
   .add("Appointment Empty", () => (
     <Fragment>
